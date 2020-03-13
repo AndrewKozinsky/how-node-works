2d8f7893f7e48e25ca6e8bc4516d0c749c3e1672
Andrew Kozinsky <andkozinskiy@yandex.ru>
2020-03-13 13:24:18 +0500

    Basic routing created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 ///////////////////////////
 // FILES
@@ -35,7 +36,22 @@ console.log('Will read file');*/
 ///////////////////////////
 // SERVER
 let server = http.createServer((req, res) => {
-    res.end('Hello from the server')
+    console.log(req.url);
+
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW')
+    } else if(pathName === '/product') {
+        res.end('This is the PRODUCT')
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>')
+    }
+
 });
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to request on port 8000');
